456689f48934afced94a7e602c10d72f2f153068
aexg <29448170+aexg@users.noreply.github.com>
2019-07-30 21:05:57 +0200

    Learn day 1

---
+++ README.md
@@ -0,0 +1,13 @@
+# Seven Languages in Seven Weeks - Prolog
+
+## Day 1: An Excellent Driver (2019-07-30)
+
+### To remember
+
+- gprolog v1.4.5 has a bug producing "existence_error" discussed in [stackoverflow thread](https://stackoverflow.com/questions/47107786/prolog-existence-error-procedure-in-basic-example). Using swi-prolog v7.6.4.
+
+### Code created
+
+- `books.pl`: a knowledge base for books with authors
+
+(end of text)
---
+++ day1/books.pl
@@ -0,0 +1,12 @@
+author(betsy_beyer,site_reliability_engineering).
+author(bruce_tate,seven_languages_in_seven_weeks).
+author(kent_beck,extreme_programming_explained).
+author(kent_beck,test-driven_development_by_example).
+author(martin_fowler,planning_extreme_programming).
+author(martin_fowler,refactoring).
+author(paul_duvall,continuous_integration).
+author(sam_newman,building_microservices).
+author(sam_newman,monolith_to_microservices).
+author(tom_demarco,adrenaline_junkies).
+author(tom_demarco,peopleware).
+author(tom_demarco,the_deadline).
